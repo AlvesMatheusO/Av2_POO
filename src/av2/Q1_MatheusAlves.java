6562ff53bf70f53db829f0b9f42522115967cd2f
Matheus Alves <matheusalv.oliveira@gmail.com>
2023-04-14 14:06:49 -0300

    exercicios ate a q5

---
+++ src/av2/Q1_MatheusAlves.java
@@ -0,0 +1,17 @@
+package av2;
+
+import java.util.Scanner;
+
+public class Q1_MatheusAlves {
+	public static void main (String [] args) {
+		Scanner screen = new Scanner(System.in);
+		
+		System.out.println("Digite o numero 1 para soma");
+		int a = screen.nextInt();
+		
+		System.out.println("Digite o numero 2 para soma");
+		int b = screen.nextInt();		
+		
+		System.out.println("O resultado da soma eh = " + (a+b));
+	}
+}
